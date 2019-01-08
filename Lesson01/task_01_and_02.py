676476e6594ccca350e53e7c9834c50bf48c445a
Vladimir Atanov <atanov.v@gmail.com>
2019-01-08 11:01:58 +0200

    Update task_01_and_02.py

--- Lesson01/task_01_and_02.py
+++ Lesson01/task_01_and_02.py
@@ -16,6 +16,8 @@ class Comment:
 
 class Book:
     def __init__(self, author, name, year, genre, *comments):
+        # Another way:
+        # def __init__(self, author, name, year, genre, comments=[]):
         self.author = author
         self.name = name
         self.year = year
@@ -23,7 +25,10 @@ class Book:
         self.comments = comments
 
     def __eq__(self, other):
-        return self.author == other.author and self.name == other.name and self.year == other.year and self.genre == other.genre
+        return self.author == other.author and \
+               self.name == other.name and \
+               self.year == other.year and \
+               self.genre == other.genre
         # Another way:
         # if self.author == other.author and self.name == other.name and self.year == other.year and self.genre == other.genre:
         #     return True
@@ -34,20 +39,30 @@ class Book:
         return 'Book({}, {}, {}, {})'.format (self.author, self.name, self.year, self.genre)
 
     def __str__(self):
-        return 'Book: author: {}, name: {}, year: {}, genre: {}, comments: {}'.format (self.author, self.name, self.year, self.genre, self.comments)
+        return 'Book: author: {}, name: {}, year: {}, genre: {}, comments: {}'.format (
+            self.author,
+            self.name,
+            self.year,
+            self.genre,
+            self.comments
+        )
 
 
 python_crash_course = Book ('Eric Matthers', 'Python crash course', 2017, 'Education')
 learning_python = Book ('Mark Lutz', 'Learning Python', 2011, 'Education')
 learning_python2 = Book ('Mark Lutz', 'Learning Python', 2011, 'Education')
 
-comms = [Comment('comment 1'), Comment('comment 2'), Comment('comment 3')]
-hh_guide_galxy = Book ('Adams Douglas', "The hitch hacker's guide to the galaxy", 1979, 'Science fiction', comms)
+comments = [Comment ('comment 1'), Comment ('comment 2'), Comment ('comment 3')]
+hh_guide_galaxy = Book ('Adams Douglas',
+                        "The hitch hacker's guide to the galaxy",
+                        1979,
+                        'Science fiction',
+                        comments)
 
 print (repr (python_crash_course))
 print (python_crash_course)
 
 print (learning_python == learning_python2)
-print (python_crash_course == hh_guide_galxy)
+print (python_crash_course == hh_guide_galaxy)
 
-print (hh_guide_galxy)
+print (hh_guide_galaxy)
